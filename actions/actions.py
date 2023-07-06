079b2ebf7e7c21d659274b5b253e2d5377be3c48
John Trammell <j.trammell@rasa.com>
2023-07-05 19:48:27 -0500

    remove redundant tests

--- actions/actions.py
+++ actions/actions.py
@@ -57,18 +57,3 @@ class ValidateMACAddressForm(FormValidationAction):
         dispatcher.utter_message(text="I still don't see a MAC address.")
         dispatcher.utter_message(text="Please contact customer support at 555-1212.")
         return {"mac_address": None, "requested_slot": None}
-
-
-if __name__ == "__main__":
-    tests = [
-        "11:22:33:44:55:66",
-        "aa:bb:cc:dd:ee:ff",
-        'my mac address is "aa:bb:cc:dd:ee:ff"',
-        'my mac address is "aa:BB:cC:dD:EE:11"',
-        'my mac address is "aa:BB:cC:dD:EE:11:gg"',
-        'my mac address is "aa:BB:cC:dD:EE:11gg"',
-        '"aa:bb:cc:dd:ee:ff"',
-    ]
-
-    for t in tests:
-        print(f"'{t}' ==> '{get_mac(t)}'")
